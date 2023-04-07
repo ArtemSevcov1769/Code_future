55d5f2b6f63cc67d7ef2c9cf1c74917c98c043bf
ArtemSevcov1769 <artemsevcov349@gmail.com>
2023-04-08 00:49:39 +0500

    v1.0.0

--- vk_bot/vk_bot.py
+++ vk_bot/vk_bot.py
@@ -1,18 +1,32 @@
+import random
 import vk_api
-import test
+from vk_api.longpoll import VkLongPoll, VkEventType
+from course import get_course
+from wiki import get_article
+import Parsing
 with open("Key.txt") as file:
     token = file.readline()
 
-vk = vk_api.VkApi(token=token)
-while True:
-    messages = vk.method('messages.getConversations', {'count' : 20, 'filter':'unanswered'})
-    if messages['count'] >= 1:
-        print(messages)
-        user_id = messages['items'][0]['last_message']['from_id']
-        message_text = messages['items'][0]['last_message']['text']
-        random_id = messages['items'][0]['last_message']['id']
-        vk.method('messages.send', {'user_id': user_id, 'random_id': random_id, 'message': '1234'})
-        if message_text.lower() == 'курс':
-            vk.method('messages.send', {'user_id': user_id, 'random_id': random_id, 'message': test.getCourse('R01235')})
+vk_session = vk_api.VkApi(token=token)
+vk = vk_session.get_api()
+upload = vk_api.VkUpload(vk)
+longpoll = VkLongPoll(vk_session)
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        msg = event.text.lower()
+        print(msg)
+        user_id = event.user_id
+        random_id = random.randint(1, 10**10) # <----
+        if msg == "планеты":
+            vk.messages.send(user_id=user_id, random_id=random_id, message = Parsing.planet(Parsing.urls[0]))
+        elif msg == "-к":
+            responce = f"{get_course('R01235')} рублей за доллар"
+            vk.messages.send(user_id=user_id, random_id=random_id, message = responce)
+        elif msg.startswith("-в"):
+            article = msg[2:]
+            responce = get_article(article=article)
+            vk.messages.send(user_id=user_id, random_id=random_id, message = responce)
         else:
-            vk.method('messages.send', {'user_id': user_id, 'random_id': random_id, 'message': 'неизвестная команда'})
+            responce = f"неизвестная команда"
+            vk.messages.send(user_id=user_id, random_id=random_id, message = responce)

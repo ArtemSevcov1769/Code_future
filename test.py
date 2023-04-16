c92cabff1fbf4f9f67f21e292431a7c81669329d
ArtemSevcov1769 <artemsevcov349@gmail.com>
2023-04-16 17:21:17 +0500

    v1.0.1

--- test.py
+++ test.py
@@ -1,33 +1,69 @@
-import time
-# my_iter = [time.sleep(x) for x in range(1, 3)]
-# print(my_iter)
-# my_iter = (time.sleep(x) for x in range(4, 6))
-# print(my_iter)
-#
-# for elem in my_iter:
-#     print('Pause')
-#     print(elem)
-#     print('Continue')
-# my_range = range(1, 100)
-# print(list(my_range))
+import pygame, random, math
 
-# def lazy_func():
-#     for i in range(1, 11):
-#         print(f'До {i}')
-#         yield i
-#         print(f'После {i}')
-#
-# for i in lazy_func():
-#     print(i)
-#     print('fbhfasdfjanf')
-#     # time.sleep(3)
-#
-# def lazy_calc(items):
-#     for item in items:
-#         yield item
-#
-# i = [1, 2, 3, 4, 5, 6, 'a', 'b']
-# for item in lazy_calc(i):
-#     print(item)
+pygame.init()
 
+window_width = 700
+window_height = 500
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption('Сквозь вселенную')
 
+background = pygame.transform.scale(pygame.image.load('black.png'), (window_width, window_height))
+
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+purple = (139, 0, 139)
+yellow = (255, 255, 0)
+
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, player_x, player_y, size_x, size_y, speed, surface):
+        super().__init__()
+        colors = [red, green, blue, purple, yellow]
+        self.image = pygame.Surface((size_x, size_y))
+        self.image.fill(random.choice(colors))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        self.surface = surface
+
+class Enemy(GameSprite):
+    def __init__(self, player_x, player_y, size_x, size_y, speed, surface):
+        super().__init__(player_x, player_y, size_x, size_y, speed, surface)
+        self.angle = random.uniform(0, 360)
+
+    def update(self):
+        x = math.cos(math.radians(self.angle)) * self.speed
+        y = math.sin(math.radians(self.angle)) * self.speed
+        self.rect.x += x
+        self.rect.y += y
+        if self.rect.left < 0 or self.rect.right > window_width or self.rect.top < 0 or self.rect.bottom > window_height:
+            self.rect.x = random.randint(349, 351)
+            self.rect.y = random.randint(249, 251)
+            self.angle = random.uniform(0, 360)
+
+enemy_size = 2
+enemy_speed = 2
+enemies = pygame.sprite.Group()
+for i in range(80):
+    enemy = Enemy(random.randint(349, 351), random.randint(249, 251), enemy_size, enemy_size, enemy_speed, window)
+    enemies.add(enemy)
+
+clock = pygame.time.Clock()
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    window.blit(background, (0, 0))
+
+    enemies.update()
+    enemies.draw(window)
+
+    pygame.display.update()
+
+    clock.tick(60)
+
+pygame.quit()
